ee35282a59bcdce2ca478ed1e158f37fc2e4180c
ncecconi <nick.cecconi@gmail.com>
2017-06-10 08:51:35 -0500

    de-couple bios, added schedule/ceremony/reception

---
+++ NickAndKatieWedding/Scripts/partydisplay.js
@@ -0,0 +1,13 @@
+﻿$('.bio span').hide();
+
+$('.bio img').click(function (e) {
+
+    e.preventDefault();
+    // hide all span
+    var $this = $(this).parent().find('span');
+    $(".bio span").not($this).hide();
+
+    // here is what I want to do
+    $this.toggle();
+
+});
