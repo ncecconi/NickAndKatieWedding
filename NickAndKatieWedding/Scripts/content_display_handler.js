6db97d75552abba0a2107a044c90fdba1bf1d0f6
ncecconi <nick.cecconi@gmail.com>
2017-06-23 12:30:22 -0500

    Refactored wedding party section - still a defect to display the default screen - requires additional UI work on bio format

---
+++ NickAndKatieWedding/Scripts/content_display_handler.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function () {
+    $('#' + $('default').data('rel')).fadeIn('slow');
+});
+
+    $(".link").click(function (e) {
+        e.preventDefault();
+        $('.content-container div').fadeOut('slow');
+        $('#' + $(this).data('rel')).fadeIn('slow');
+    });
