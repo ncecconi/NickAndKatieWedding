d9c3e8c18c184e92354d79407828eee95da18888
ncecconi <nick.cecconi@gmail.com>
2017-12-01 10:31:35 -0600

    Corrected countdown timer

--- NickAndKatieWedding/Scripts/countdown_timer.js
+++ NickAndKatieWedding/Scripts/countdown_timer.js
@@ -1,4 +1,4 @@
-﻿var countDownDate = new Date("Dec 2, 2017 15:00:00").getTime();
+﻿var countDownDate = new Date("Dec 2, 2017 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
